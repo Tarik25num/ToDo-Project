9ee14d11b692e890c081a3789b0f92ccbdf8982e
jha <j.haimez@vattenfall.fr>
2025-07-11 08:50:04 +0200

    init

---
+++ auth/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import {Login} from '../login/login';
+import {authGuard} from '../auth-service/authguard';
+
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('../dashboard/dashboard').then(m => m.Dashboard)
+  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
